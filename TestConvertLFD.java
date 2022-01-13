20dfdfb2b191e0738801e465b0f5366e65921f73
Dingli Zhang <dingli@iscas.ac.cn>
2022-01-13 14:17:13 +0800

    Add TestConvertLFD.java for convert between long and float/double (#4)

---
+++ TestConvertLFD.java
@@ -0,0 +1,26 @@
+/**
+ * Test case for convert between long and float/double
+ * @author Dingli Zhang
+ * @version 1.0
+ */
+public class TestConvertLFD {
+    /**
+     * Convert between long and float/double
+     * By the way, the correct result of examples in the following is:
+     *
+     * test l2f in: -64424509696 out: -6.4424509E10
+     * test l2d in: -64424509696 out: -6.4424509696E10
+     * test f2l in: -6.4424509E10 out: -64424509440
+     * test d2l in: -6.4424509696E10 out: -64424509696
+     */
+    public static void main(String[] args) {
+        long a = 0xFFFFFFF0FFFFFF00L;
+        float b = -64424509696F;
+        double c = -64424509696D;
+
+        System.out.println("test l2f in: "+a+" out: "+ (float)a );
+        System.out.println("test l2d in: "+a+" out: "+ (double)a);
+        System.out.println("test f2l in: "+b+" out: "+ (long)b);
+        System.out.println("test d2l in: "+c+" out: "+ (long)c);
+    }
+}
