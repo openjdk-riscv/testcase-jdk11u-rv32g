603c51fe04a98a12a9d1f6b7005794ebbd6c72b1
Dingli Zhang <dingli@iscas.ac.cn>
2022-01-13 14:09:24 +0800

    Add TestLushr.java for lushr test case (#3)

---
+++ TestLushr.java
@@ -0,0 +1,16 @@
+/**
+ * Test case for lush
+ * @author Dingli Zhang
+ * @version 1.0
+ */
+public class TestLushr {
+    /**
+     * Logical shift right long.
+     * By the way, the correct result of examples in the following is 15.
+     */
+    public static void main(String[] args) {
+        long a = -1L;
+        int b = -68;
+        System.out.println(a>>>b);
+    }
+}
