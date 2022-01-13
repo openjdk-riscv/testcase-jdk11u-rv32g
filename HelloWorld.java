bb5ad0985273f74677212f53ae72a9438b764442
zhangxiang-plct <zhangxiang@nj.iscas.ac.cn>
2022-01-13 18:46:13 +0800

    Add HelloWorld.java and TestPrintLong.java for test case

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,12 @@
+/**
+ * Test case for hello world
+ * @author Ningning Shi
+ * @version 1.0
+ */
 public class HelloWorld {
+    /**
+     * @return By the way, the correct result of examples in the following is "Hello World".
+     */
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
