4058f4364a5804964d123c77f6c804e846d184e0
zifeihan <caogui@iscas.ac.cn>
2022-01-17 17:35:09 +0800

    Polishing.

--- Launcher.java
+++ Launcher.java
@@ -1,27 +1,30 @@
 import java.lang.reflect.Method;
 
+/**
+ * Test launcher
+ */
 public class Launcher {
 
+    // notice we need add test case to testCase array.
+    static String[] testCase = new String[] {
+        "HelloWorld",
+        "TestArrayOverflow",
+        "TestConvertIFD",
+        "TestConvertLFD",
+        "TestLadd",
+        "TestLastore",
+        "TestLdiv",
+        "TestLmul",
+        "TestLneg",
+        "TestLrem",
+        "TestLshl",
+        "TestLshr",
+        "TestLsub",
+        "TestLushr",
+        "TestLxor"
+    };
+
     public static void main(String[] args) throws Exception {
-
-        String[] testCase = new String[] {
-            "HelloWorld",
-            "TestArrayOverflow",
-            "TestConvertIFD",
-            "TestConvertLFD",
-            "TestLadd",
-            "TestLastore",
-            "TestLdiv",
-            "TestLmul",
-            "TestLneg",
-            "TestLrem",
-            "TestLshl",
-            "TestLshr",
-            "TestLsub",
-            "TestLushr",
-            "TestLxor"
-        };
-
         System.out.println("run test start...");
         for (String str : testCase) {
             runTest(str);
