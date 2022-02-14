395efbbae570e3cd53bcd02a342df683bb05ff73
zifeihan <caogui@iscas.ac.cn>
2022-02-14 09:35:32 +0800

    Polishing.

--- run_test.sh
+++ run_test.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-export JAVA_HOME=/Users/zifeihan/program/zulu-11.jdk
+# this JAVA_HOME should be RV32G java home path
+export JAVA_HOME=
+
+if [ ! -n "$JAVA_HOME" ];then
+    echo "Please designate JAVA_HOME path."
+    exit 1
+fi
 echo "JAVA_HOME:" ${JAVA_HOME}
 
 bash ./mvnw clean test
